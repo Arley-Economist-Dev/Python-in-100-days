20d15e38db13e4211c6b6d817d9dea804750b914
Arley Paredes <arleyjesus7@gmail.com>
2023-01-19 09:46:54 -0600

    finalizacion dia 4

--- Beginner/4-proyecto.py
+++ Beginner/4-proyecto.py
@@ -117,3 +117,56 @@ selected_row[columna - 1] = "X"
 map[fila - 1][columna - 1] = "X"
 
 print(f"{row1}\n{row2}\n{row3}")
+
+
+#Proyecto piedra papel o tijeras
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+elementos = [rock, paper, scissors]
+seleccion = elementos [ int( input("Bienvenido a Piedra, Papel o Tijeras, ¿Cual eliges?\n (0 piedra, 1 papel, 2 tijeras): ") ) ]
+aleatoria = random.choice(elementos)
+
+print(f"Tu seleccionaste: \n {seleccion}")
+print(f"La computadora selecciono: \n {aleatoria}")
+
+if aleatoria == rock and seleccion == scissors:
+    print("Pierdes la piedra rompe a la tijera")
+elif aleatoria == rock and seleccion == paper:
+    print("Ganas porque el papel envuelve a la piedra")
+elif aleatoria == paper and seleccion == rock:
+    print("Pierdes porque el papel envuelve a la piedra")
+elif aleatoria == paper and seleccion == scissors:
+    print("Ganas porque la tijera corta el papel")
+elif aleatoria == scissors and seleccion == rock:
+    print("Ganas porque la piedra rompe a la tijera")
+elif aleatoria == scissors and seleccion == paper:
+    print("Pierdes porque el papel es cortado por la tijera")
+else:
+    print("Eligieron lo mismo por lo tanto es un empate.")
--- Beginner/main.py
+++ Beginner/main.py
@@ -1,24 +1,51 @@
-#Reto de juego para encontrar un tesoro en alguna posicion del tablero
-row1 = ["⬜","⬜","⬜"]
-row2 = ["⬜","⬜","⬜"]
-row3 = ["⬜","⬜","⬜"]
+#Proyecto piedra papel o tijeras
+import random
 
-#se crea una nested list para esta cuadricula
-map = [row1, row2, row3]
-#con esto se imprime la cuadricula
-print(f"{row1}\n{row2}\n{row3}")
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-#aqui pedimos el input necesario para el juego
-position = input("Where do you want to put the treasure?\n(first digit is column second is row): ")
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-#estas variables almacenan en numero los dos digitos
-columna = int ( position[0] ) #2
-fila = int ( position[1] ) #3
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
 
-#se resta uno al valor de fila para evadir el problema de indices
-selected_row = map[fila - 1]
-#se navega hasta la ultima lista anidada del conjunto y se busca su indice en funcion de la columna
-#este elemento de esa coordenada es reemplazado por una X
-selected_row[columna - 1] = "X"
+elementos = [rock, paper, scissors]
+seleccion = elementos [ int( input("Bienvenido a Piedra, Papel o Tijeras, ¿Cual eliges?\n (0 piedra, 1 papel, 2 tijeras): ") ) ]
+aleatoria = random.choice(elementos)
 
-print(f"{row1}\n{row2}\n{row3}")
+print(f"Tu seleccionaste: \n {seleccion}")
+print(f"La computadora selecciono: \n {aleatoria}")
+
+if aleatoria == rock and seleccion == scissors:
+    print("Pierdes la piedra rompe a la tijera")
+elif aleatoria == rock and seleccion == paper:
+    print("Ganas porque el papel envuelve a la piedra")
+elif aleatoria == paper and seleccion == rock:
+    print("Pierdes porque el papel envuelve a la piedra")
+elif aleatoria == paper and seleccion == scissors:
+    print("Ganas porque la tijera corta el papel")
+elif aleatoria == scissors and seleccion == rock:
+    print("Ganas porque la piedra rompe a la tijera")
+elif aleatoria == scissors and seleccion == paper:
+    print("Pierdes porque el papel es cortado por la tijera")
+else:
+    print("Eligieron lo mismo por lo tanto es un empate.")
